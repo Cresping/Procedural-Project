d181a1bddd6864354a8d932470442c6aa63fc5b6
Fran_Sanchez <fsanchez.mbox@uma.es>
2021-06-18 17:08:43 +0200

    Comit

---
+++ Assets/Scripts/SO/MazeVariableSO.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace HeroesGames.ProjectProcedural.SO
+{
+    [CreateAssetMenu(fileName = nameof(MazeVariableSO), menuName = "Scriptables/" + nameof(MazeVariableSO) + "/" + "MazeVariable")]
+    public class MazeVariableSO : DungeonVariableSO
+    {
+        private void MazeSize()
+        {
+            if (dungeonLvl % 2 == 0)
+            {
+                dungeonWidth = 15 + dungeonLvl + 1;
+                dungeonHeight = 15 + dungeonLvl + 1;
+            }
+            else
+            {
+                dungeonWidth = 15 + dungeonLvl;
+                dungeonHeight = 15 + dungeonLvl;
+            }
+
+        }
+        
+        public override void CalculateDifficulty()
+        {
+            MazeSize();
+        }
+    }
+}
+
+
