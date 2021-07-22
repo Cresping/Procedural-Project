c9dd8e936455913a7192ff902a58e50b3663f0f2
DESKTOP-STK4PLB\Cresping <crespingqw@gmail.com>
2021-07-22 19:11:13 +0200

    Se ha agregado una pantalla de error

---
+++ Assets/Scripts/UI/UIPlayfabErrorController.cs
@@ -0,0 +1,39 @@
+using HeroesGames.ProjectProcedural.SO;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+namespace HeroesGames.ProjectProcedural.UI
+{
+    public class UIPlayfabErrorController : MonoBehaviour
+    {
+        [SerializeField] private PlayfabBusSO playfabBusSO;
+        [SerializeField] private TextMeshProUGUI errorText;
+        [SerializeField] private GameObject errorPanel;
+
+        private void Awake()
+        {
+            errorPanel.SetActive(false);
+        }
+        private void OnEnable()
+        {
+            playfabBusSO.OnErrorPlayFab += OnError;
+        }
+        private void OnDisable()
+        {
+            playfabBusSO.OnErrorPlayFab -= OnError;
+        }
+        private void OnError(string errorText)
+        {
+            this.errorText.text = errorText;
+            this.errorPanel.SetActive(true);
+        }
+        public void DisableWindows()
+        {
+            this.errorPanel.SetActive(false);
+        }
+    }
+}
+
